3c2ca39278a8c80a3899a34eb7a2ef191743fa7e
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-02-11 18:14:10 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,9 +4,6 @@ import http from 'http';
 import {Server} from 'socket.io';
 
 const port = process.env.PORT || 8080;
-// const CORS_ORIGIN = process.env.WEB_APP_ORIGIN || 'http://localhost:3000';
-
-// console.log('cors origin', CORS_ORIGIN);
 
 const app = express();
 const server = http.createServer(app);
@@ -32,44 +29,42 @@ app.use(express.urlencoded({ extended: true }));
 //     res.send({ link: uuidV4() });
 // });
 
+// handler when a client connects to the socket server
 io.on('connection', socket => {
     console.log('user connected');
 
+    // join a room so you can send socket messages to all other people only within that room
     socket.on('join-room', (userData) => {
+        // userData object defined by client
         const { socketId, roomId, name, status } = userData;
 
         console.log(`user joined ${roomId}`, userData);
 
         socket.join(roomId);
+        
+        // inform all other users you have connected
         io.in(roomId).emit('new-user-connect', userData);
+        
+        // send your peer.js id to other users so that they can video call you via the peer.js call() api
         socket.on('peer-id-offer', (peerIdData) => {
             console.log('peer-id-offer');
             socket.to(roomId).emit('peer-id-received', peerIdData);
         });
+        // send a message to all other users
         socket.on('chat-message', (chatData) => {
             console.log('chat message');
             io.to(roomId).emit('chat-message', chatData);
         });
+        // provide other users with your current information
         socket.on('update', () => {
             console.log('user update');
             io.to(roomId).emit('user-update', userData);
         });
+        // when user disconnects, inform all other users they have disconnected
         socket.on('disconnect', () => {
             console.log('user disconnected');
             io.in(roomId).emit('user-disconnected', userData);
         });
-//         socket.on('broadcast-message', (message) => {
-//             io.in(roomId).emit('new-broadcast-messsage', {...message, userData});
-//         });
-        // socket.on('reconnect-user', () => {
-        //     socket.to(roomID).broadcast.emit('new-user-connect', userData);
-        // });
-//         socket.on('display-media', (value) => {
-//             io.in(roomId).emit('display-media', {socketId, value });
-//         });
-//         socket.on('user-video-off', (value) => {
-//             io.in(roomId).emit('user-video-off', value);
-//         });
     });
 });
 
